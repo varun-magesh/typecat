bf7f2767651be3e37ffb11ebbf84f79d9e1f90a9
Varun Iyer <vi.mail@protonmail.ch>
2017-05-23 14:22:05 -0700

    Fixed anchoring for fontboxes

--- display/fontbox.py
+++ display/fontbox.py
@@ -7,11 +7,11 @@ from gi.repository import Gtk, Pango
 
 class FontBox(Gtk.Box):
 
-    def __init__(self, font, text="Handgloves", size=(300, 200), font_size=75):
+    def __init__(self, font, text="Handgloves", size=(200, 150), font_size=75):
         Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL)
-
+        self.set_border_width(10)
         self.font = font
-        self.font_size = 35
+        self.font_size = int(size[0]/9)
         self.font.set_size(self.font_size)
         self.text = text
 
@@ -21,7 +21,7 @@ class FontBox(Gtk.Box):
         self.title.set_markup("<b>{}</b>".format(self.font.name))
         self.pack_start(self.title, False, False, 0)
 
-        self.image = Gtk.Image()
+        self.image = Gtk.Image(halign=Gtk.Align.START)
         self.image.set_from_pixbuf(f2i.multiline_gtk(self.text, self.font.pilfont, self.size))
         self.pack_start(self.image, False, False, 0)
 
