f598736184a739c15839e7baf88617a9898919f2
Timothy Kanarsky <tkanarsky@outlook.com>
2017-05-23 14:21:57 -0700

    Changes to the font crawler (more categories, etc)

--- scrapy/tutorial/tutorial/settings.py
+++ scrapy/tutorial/tutorial/settings.py
@@ -27,9 +27,9 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1
+# DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 1
+# CONCURRENT_REQUESTS_PER_DOMAIN = 1
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
@@ -68,7 +68,7 @@ ITEM_PIPELINES = {
    'tutorial.pipelines.FontZipPipeline': 1,
 }
 
-FILES_STORE = '/home/timothy/dl_fonts'
+FILES_STORE = '/home/apcs/dl_fonts/script'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
