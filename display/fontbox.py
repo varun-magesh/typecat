2c6dfc0ad6a6b478832edb21e848b72d714e76d1
Varun Iyer <vi.mail@protonmail.ch>
2017-05-25 21:52:20 -0700

    Made the fonts be in frames

--- display/fontbox.py
+++ display/fontbox.py
@@ -7,7 +7,7 @@ class FontBox(Gtk.FlowBoxChild):
 
     def __init__(self, font, text="Handgloves", size=(200, 150), font_size=75):
         Gtk.FlowBoxChild.__init__(self)
-        self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.frame = Gtk.Frame()
         self.set_border_width(5)
         self.font = font
         self.font_size = int(size[0]/9)
@@ -16,14 +16,15 @@ class FontBox(Gtk.FlowBoxChild):
 
         self.size = size
 
-        self.title = Gtk.Label(halign=Gtk.Align.START)
-        self.title.set_markup("<b>{}</b>".format(self.font.name))
-        self.title.set_ellipsize(True)
-        self.box.pack_start(self.title, False, False, 0)
+        self.frame.set_label(self.font.name)
+        self.frame.set_label_align(.2, .5)
 
-        self.image = Gtk.Image(halign=Gtk.Align.START)
+        self.box = Gtk.Box()
+        self.box.set_border_width(5)
+        self.image = Gtk.Image(halign=Gtk.Align.CENTER)
         self.image.set_from_pixbuf(f2i.multiline_gtk(self.text, self.font.pilfont, self.size))
-        self.box.pack_start(self.image, False, False, 0)
+        self.box.pack_start(self.image, True, False, 0)
+        self.frame.add(self.box)
 
-        self.add(self.box)
+        self.add(self.frame)
 
