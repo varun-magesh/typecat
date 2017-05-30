d8a58e57b78c1f14fa7585d22b0e2496b9c8d515
Timothy Kanarsky <tkanarsky@outlook.com>
2017-05-29 17:48:35 -0700

    Updated model, unfortunately doesn't work well

--- machine_learning/eval.py
+++ machine_learning/eval.py
@@ -4,10 +4,10 @@ import machine_learning.font2tensor as f2t
 from machine_learning.labels import font_labels
 import os
 
-EVAL_FONT_DIR = '/usr/share/fonts'
+EVAL_FONT_DIR = '/home/timothy/dl_fonts'
 
 SAVE_DIR = os.path.dirname(os.path.realpath(__file__)) + '/training_data/'
-META_FILE = 'model.ckpt-200.meta'
+META_FILE = 'model.ckpt-480.meta'
 
 NUM_TO_EVAL = mdl.BATCH_SIZE
 
@@ -21,10 +21,12 @@ with model_to_eval.as_default():
         saver.restore(sess, tf.train.latest_checkpoint(SAVE_DIR))
         init = tf.global_variables_initializer()
         sess.run(init)
-        generator = f2t.generate_batch(NUM_TO_EVAL, font_labels, EVAL_FONT_DIR, False)
-        predictions = tf.argmax(mdl.logits, 1)
-        images_feed = generator.__next__()
+        generator = f2t.generate_batch(NUM_TO_EVAL, font_labels, EVAL_FONT_DIR, True)
+        predictions = tf.equal(tf.argmax(mdl.logits, 1), tf.argmax(mdl.labels_placeholder, 1))
+        accuracy = tf.reduce_mean(tf.cast(predictions, tf.float32))
+        images_feed, labels_feed = generator.__next__()
         feed_dict = {
             mdl.fonts_placeholder: sess.run(images_feed),
+            mdl.labels_placeholder: sess.run(labels_feed)
         }
-        print(sess.run(predictions, feed_dict=feed_dict))
+        print(sess.run(accuracy, feed_dict=feed_dict))
